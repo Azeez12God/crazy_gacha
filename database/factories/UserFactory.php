dfd86df533d52af5702b341148be404cfb9d9c74
Alberto <albertolopez.alu@iespacomolla.es>
2025-01-17 19:47:39 +0100

    Finalizadas relaciones de base de datos

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -26,8 +26,9 @@ class UserFactory extends Factory
         return [
             'name' => fake()->name(),
             'email' => fake()->unique()->safeEmail(),
-            'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
+            'money' => fake()->numberBetween(0, 1000),
+            'profile_picture' => fake()->imageUrl(),
             'remember_token' => Str::random(10),
         ];
     }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,11 +13,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        $this->call([
+            UserSeeder::class,
+            PrizesSeeder::class,
+            ShopSeeder::class,
+            Shop_UserSeeder::class,
+            Prize_UserSeeder::class,
         ]);
     }
 }
